dcb9b88c5fe532c287429bc854f9028761647723
Michael D'Silva <michael.dsilva.343@gmail.com>
2021-02-18 17:42:08 -0500

    implemented player stats
    followed this tutorial: https://www.youtube.com/watch?v=e8GmfoaOB4Y&ab_channel=Brackeys

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+    
+    public Stat damage;
+    public Stat armour; 
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+        currentHealth -= damage;
+
+        Debug.Log(transform.name + " takes" + damage + " damage");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " has died.");
+    }
+}
---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChangedCallback += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newEquipment, Equipment oldEquipment)
+    {
+        if (newEquipment != null)
+        {
+            armour.AddModifier(newEquipment.armorModifier);
+            damage.AddModifier(newEquipment.damageModifier);
+        }
+
+        if (oldEquipment != null)
+        {
+            armour.RemoveModifier(oldEquipment.armorModifier);
+            damage.RemoveModifier(oldEquipment.damageModifier);
+        }
+    }
+}
